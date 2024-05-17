b1ae5cfbdf5072a31755d3198dee3e434bc8e19e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-05-16 18:52:41 -0700

    Save tokenized lines in array

--- src/main.c
+++ src/main.c
@@ -22,31 +22,37 @@ int main(int argc, char **argv) {
   // starting at 2 to account for `\n` and `\0`
   int cur_line_size = 2;
   int max_line_size = 2;
+  int line_count = 1;
+  int is_last_line_empty = 1;
   while (!feof(file)) {
-    if (fgetc(file) == '\n') {
+    char cur_c = fgetc(file);
+    if (cur_c == '\n') {
       cur_line_size = 2;
+      line_count += 1;
+      is_last_line_empty = 1;
     } else {
       cur_line_size += 1;
       if (cur_line_size > max_line_size) {
         max_line_size = cur_line_size;
       }
+      if (is_letter(cur_c)) {
+        is_last_line_empty = 0;
+      }
     }
+  }
+  if (is_last_line_empty) {
+    line_count -= 1;
   }
   fseek(file, 0, SEEK_SET);
 
-  // read a line at a time
-  while (!feof(file)) {
+  // read a line at a time and tokenize
+  Token tokenized_lines[line_count][max_line_size];
+  for (int i = 0; i < line_count; i++) {
     char line[max_line_size];
     fgets(line, max_line_size, file);
     to_lower(line);
-    int i = 0;
-
     int token_count = count_tokens(line);
-    Token tokens[token_count];
-    tokenize_line(tokens, token_count, line);
-    for (int i = 0; i < token_count; i++) {
-      printf("TOKENS %d, %s\n", tokens[i].type, tokens[i].value);
-    }
+    tokenize_line(tokenized_lines[i], token_count, line);
   }
 
   fclose(file);
